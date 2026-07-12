76d1dd2fec377fead787eb8ba140ddae232d06b0
Julius Brussee <104168679+JuliusBrussee@users.noreply.github.com>
2026-07-12 16:24:57 +0200

    fix(windows): expose native SDK headers

--- Sources/CPebblePlatform/PBAudio.c
+++ Sources/CPebblePlatform/PBAudio.c
@@ -107,6 +107,7 @@ void pb_audio_destroy(PBAudioDevice *device) {
 #elif defined(_WIN32)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <mmreg.h>
 #include <mmsystem.h>
 
 #define PB_AUDIO_BUFFERS 3
